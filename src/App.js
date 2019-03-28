31d92e88f9b9f88c87b277d9d4cab41611d2f2c0
unknown <juradomartinbenedict@gmail.com>
2019-03-28 12:13:56 -0400

    Added bootstrap, Minor App component change, imported bootstrap, added counter.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      Hello
+      <div>
+        <h1>Hello</h1>   
       </div>
     );
   }

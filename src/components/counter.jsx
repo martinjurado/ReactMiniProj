7376e5405bb9f764f547003ce87de3e5a2be802d
unknown <juradomartinbenedict@gmail.com>
2019-03-28 14:04:55 -0400

    Changed Counters component, changed count to value, added Counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,36 +2,41 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
-    
+    value : this.props.value
   };
-
 
   handleIncrement = () => {
     this.setState({
-        count: this.state.count + 1
-    })
-  }
+      value: this.state.value + 1
+    });
+  };
 
   render() {
-      
+    
+    console.log(this.props)
     return (
       <div>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
